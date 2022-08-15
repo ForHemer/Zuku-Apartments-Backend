e37c25638a77c290a7448556756bd3dde0136318
Have Samuel <samiehave9@gmail.com>
2022-08-15 20:42:56 +0300

    Update User model and crreate validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
+  devise :database_authenticatable,
+         :jwt_authenticatable,
+         :registerable,
+         jwt_revocation_strategy: JwtDenylist
+
+  has_many :reservations
+
+  validates :name, presence: true
+  validates_uniqueness_of :email 
 end
