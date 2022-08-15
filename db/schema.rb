1b655192a6b9a0401bf0bb2a2a6b61a1338d951a
ForHemer <foreste.hemerson@gmail.com>
2022-08-15 13:06:05 -0400

    Create schema for database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_12_011434) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_170048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_011434) do
     t.integer "price"
     t.string "city"
     t.integer "capacity"
-    t.text "decription"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -41,9 +41,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_011434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "apartment_id", null: false
+    t.bigint "user_id", null: false
     t.index ["apartment_id"], name: "index_reservations_on_apartment_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "apartment_images", "apartments"
   add_foreign_key "reservations", "apartments"
+  add_foreign_key "reservations", "users"
 end
