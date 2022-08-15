274b71fabeacdfe7f230fbff83537b0ea3c90262
Have Samuel <samiehave9@gmail.com>
2022-08-15 20:55:01 +0300

    Create jwt_denylist migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_170929) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_174540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_170929) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
   create_table "reservations", force: :cascade do |t|
