a0f3184dd7cc06a2bae3ffa402225016125927ae
ForHemer <foreste.hemerson@gmail.com>
2022-08-23 18:01:25 -0400

    Update reservations controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::ReservationsController < ApplicationController
     @reservation = Reservation.new(reservation_params)
 
     if @reservation.save
-      render json: @reservation, status: :created, location: @reservation
+      render json: {message: 'success'}
     else
       render json: @reservation.errors, status: :unprocessable_entity
     end
@@ -47,6 +47,6 @@ class Api::V1::ReservationsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def reservation_params
-    params.require(:reservation).permit(:startDate, :endDate, :amount)
+    params.require(:reservation).permit(:startDate, :endDate, :amount, :apartment_id, :user_id)
   end
 end
